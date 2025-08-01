f548fb2fea66fd77493dd9406f5a31290b49443f
Ismaelmendes <ismaelmendes935@gmail.com>
2025-08-01 02:58:23 +0000

    Add new rote

---
+++ pages/rota-nova.js
@@ -0,0 +1,9 @@
+function NewRote() 
+{
+    return <div>
+        <h1>Rota Encontrada</h1>
+        <img src="https://i.pinimg.com/originals/27/f9/a3/27f9a37eb3dc9b7b7e8e648bb987816b.gif"></img>
+    </div>
+}
+
+export default NewRote;
