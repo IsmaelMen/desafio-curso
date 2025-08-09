8fe7756b4beb535067bdf2b234f1c2f72d4d1880
Ismaelmendes <ismaelmendes935@gmail.com>
2025-08-09 01:08:36 +0000

    Testando react components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+function NewLine()
+{
+    return (
+        <p>
+            <h2>Novo título</h2>
+        </p>
+    );
+}
+
 function Home() {
-    return <h1>Tinha uma foto de um macaco</h1>
+    return (
+        <div>
+            <NewLine />
+            <p>Primeiro Parágrafo</p>
+        </div>
+    );
 }
 
 export default Home;
